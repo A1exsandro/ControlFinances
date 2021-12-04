92a34a543ad9867774b596e0146b1fc742e9ea57
A1exsandro <alexsandro.acp@gmail.com>
2021-12-03 21:02:00 -0400

    Project created Gofinances

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function Dashboard(){
+    return (
+        <View style={styles.container}>
+            <Text>Dashboard</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center'
+    }
+})
